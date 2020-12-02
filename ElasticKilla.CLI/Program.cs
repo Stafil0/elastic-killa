9807e99c1f729ec00e41cd1e987b8aa3a29fd569
Egor Mukhachev <stafilo13@gmail.com>
2020-12-02 16:07:15 +0400

    Простенький CLI

--- ElasticKilla.CLI/Program.cs
+++ ElasticKilla.CLI/Program.cs
@@ -1,12 +1,73 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using ElasticKilla.Core.Analyzers;
+using ElasticKilla.Core.Tokenizer;
 
 namespace ElasticKilla.CLI
 {
+    internal class Inputs
+    {
+        public const string Exit = "exit";
+        
+        public const string Query = "q";
+        
+        public const string Subscribe = "sub";
+        
+        public const string Unsubscribe = "unsub";
+    }
+
     class Program
     {
-        static void Main(string[] args)
+        private const string Promt = "> ";
+
+        private static FileAnalyzer _analyzer;
+
+        private static IEnumerable<string> Search(string query) => _analyzer.Search(query);
+
+        private static async Task Subscribe(string path, string pattern) => await _analyzer.Subscribe(path, pattern);
+
+        private static async Task Unsubscribe(string path) => await _analyzer.Unsubscribe(path);
+
+        private static async Task<bool> ExecuteInput(string input)
         {
+            if (input == null)
+                return true;
+
+            var cmd = input.Split(" ");
+            switch (cmd[0])
+            {
+                case Inputs.Query:
+                    var files = Search(cmd[1]);
+                    foreach (var file in files)
+                        Console.WriteLine(file);
+                    break;
+                case Inputs.Subscribe:
+                    var path = cmd[1];
+                    var pattern = cmd.Length > 2 ? cmd[2] : string.Empty;  
+                    await Subscribe(path, pattern);
+                    break;
+                case Inputs.Unsubscribe:
+                    await Unsubscribe(cmd[1]);
+                    break;
+                case Inputs.Exit:
+                    return false;
+            }
+
+            return true;
+        }
+
+        public static async Task Main(string[] args)
+        {
+            _analyzer = new FileAnalyzer(new WhitespaceTokenizer());
+
+            bool next;
+            do
+            {
+                Console.Write(Promt);
+                var input = Console.ReadLine();
+                next = await ExecuteInput(input);
+            } while (next);
         }
     }
 }
